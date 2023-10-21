59968943134d964f61c80467a4996c56f933ac50
rattiyakorn19 <patty19.rattiyakorn@gmail.com>
2023-10-21 14:02:28 +0700

    Resolved conflicts

---
+++ java.js
@@ -0,0 +1,9 @@
+change.addEventListener('mouseover',change1);
+function change1() {
+    document.getElementById('new1').innerHTML = "RESUME"
+  }
+
+change.addEventListener('mouseout',change2);
+function change2() {
+    document.getElementById('new1').innerHTML = "Click"
+  }
